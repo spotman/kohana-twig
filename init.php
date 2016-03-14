39f85c73da240f26249264091138be170021474d
Spotman <i.am@spotman.ru>
2016-03-14 19:52:31 +0300

    composer update + sitemap + configs

--- init.php
+++ init.php
@@ -1,4 +1,4 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php defined('SYSPATH') OR die('No direct script access.');
 
 define('TWIGPATH', rtrim(dirname(__FILE__), DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR);
 Twig::init();
